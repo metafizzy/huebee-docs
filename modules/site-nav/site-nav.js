13c6b00e7ee3ecda76012d027fe57bd1916c05a5
David DeSandro <desandrocodes@gmail.com>
2016-11-12 20:54:14 -0500

    🛠 stickeroo site nav

---
+++ modules/site-nav/site-nav.js
@@ -0,0 +1,67 @@
+( function() {
+
+HuebeeDocs['site-nav'] = function( elem ) {
+
+  var navHeight = elem.offsetHeight;
+
+  // only activate if over 768px window width
+  // don't proceed if navHeight is bigger than window
+  if ( window.innerWidth < 1200 || navHeight >= window.innerHeight ) {
+    return;
+  }
+
+  new Stickeroo( elem );
+
+};
+
+// -------------------------- Stickeroo -------------------------- //
+
+// sticky elements, like the page nav
+function Stickeroo( element ) {
+  this.element = element;
+  this.originalY = this.element.getBoundingClientRect().top + window.pageYOffset;
+  window.addEventListener( 'scroll', this );
+  this.isFixed = false;
+  this.onscroll();
+}
+
+Stickeroo.prototype.handleEvent = function( event ) {
+  var methodName = 'on' + event.type;
+  if ( this[ methodName ] ) {
+    this[ methodName ]( event );
+  }
+};
+
+
+function throttleProto( _class, methodName, threshold ) {
+  // original method
+  var method = _class.prototype[ methodName ];
+  var timeoutName = methodName + 'Timeout';
+
+  _class.prototype[ methodName ] = function() {
+    if ( this[ timeoutName ] ) {
+      return;
+    }
+
+    method.apply( this, arguments );
+    var _this = this;
+    this[ timeoutName ] = setTimeout( function() {
+      method.apply( _this, arguments );
+      delete _this[ timeoutName ];
+    }, threshold || 100 );
+  };
+}
+
+Stickeroo.prototype.onscroll = function() {
+  var isFixed = window.pageYOffset >= this.originalY;
+  if ( isFixed === this.isFixed ) {
+    return;
+  }
+
+  this.element.classList.toggle('is-fixed');
+  this.isFixed = isFixed;
+};
+
+throttleProto( Stickeroo, 'onscroll', 50 );
+
+})();
